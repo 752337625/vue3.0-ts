96f03ff72955626cdb63361aa46c1b01f5aa94ec
whw <whw520byl@163.com>
2021-09-29 15:08:05 +0800

    feat: pre-commit优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,128 +7,19 @@ module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      tsx: true,
-    },
   },
   env: {
     browser: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  globals: {},
   rules: {
-    // js/ts
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': 'error',
-    'no-console': [
-      'warn',
-      {
-        allow: ['error'],
-      },
-    ],
-    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
-    camelcase: [
-      'error',
-      {
-        properties: 'never',
-      },
-    ],
-
-    'no-var': 'error',
-    'no-empty': [
-      'error',
-      {
-        allowEmptyCatch: true,
-      },
-    ],
-    'no-with': 'error',
-    'no-void': 'error',
-    'prefer-const': [
-      'warn',
-      {
-        destructuring: 'all',
-        ignoreReadBeforeAssign: true,
-      },
-    ],
-    'prefer-template': 'error',
-    'object-shorthand': [
-      'error',
-      'always',
-      {
-        ignoreConstructors: false,
-        avoidQuotes: true,
-      },
-    ],
-    'block-scoped-var': 'error',
-    'no-constant-condition': [
-      'error',
-      {
-        checkLoops: false,
-      },
-    ],
-
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        disallowTypeAnnotations: false,
-      },
-    ],
-
-    // vue
-    'vue/no-v-html': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/require-explicit-emits': 'off',
-
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-whitespace-before-property': 'error', // 禁止属性前有空白
+    'no-const-assign': 'error', // 禁止修改const声明的变量
     'prettier/prettier': 'error',
-
-    // import
-    'import/first': 'error',
-    'import/no-duplicates': 'error',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
-
-        pathGroups: [
-          {
-            pattern: 'vue',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@vue/**',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@element-plus/**',
-            group: 'internal',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['type'],
-      },
-    ],
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.vue'],
-      rules: {
-        'no-undef': 'off',
-      },
-    },
-    {
-      files: ['**/__tests__/**'],
-      rules: {
-        'no-console': 'off',
-      },
-    },
-  ],
 };
